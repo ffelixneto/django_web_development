36349b19f102cd2c6bc6a94fe6ed7d9fcc1230d7
ffelixneto <ffelixneto@outlook.com>
2022-07-06 18:58:09 -0300

    Alteração nome do projeto para livreto

--- livreto/asgi.py
+++ livreto/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for book_rev project.
+ASGI config for livreto project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'book_rev.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'livreto.settings')
 
 application = get_asgi_application()
--- livreto/wsgi.py
+++ livreto/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for book_rev project.
+WSGI config for livreto project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'book_rev.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'livreto.settings')
 
 application = get_wsgi_application()
