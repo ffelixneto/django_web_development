11a081fee7d7d08a636d34ef52ed4cb49228102f
ffelixneto <ffelixneto@outlook.com>
2022-07-13 11:40:31 -0300

    Adicionado template de busca por termo.

--- livreto/urls.py
+++ livreto/urls.py
@@ -21,4 +21,5 @@ import reviews.views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', reviews.views.index),
+    path('pesquisa_livro/', reviews.views.pesquisa_livro),
 ]
--- reviews/views.py
+++ reviews/views.py
@@ -8,3 +8,14 @@ def index(request):
     name = "Sol"
     
     return render(request, "base.html", {"name" : name})
+
+def pesquisa_livro(resquest):
+    '''
+    FUNÇÃO PARA REALIZAR A BUSCA DE LIVROS POR UM TERMO PASSADO PELO USUÁRIO\n
+    O contexto busca pela chave `termo_busca` e a envia pra o contexto.
+    '''
+
+    termo_busca = resquest.GET.get("termo_busca") or "termo não informado :/"
+
+    return render(resquest, "resultado_pesquisa.html", {"termo_busca" : termo_busca})
+    
